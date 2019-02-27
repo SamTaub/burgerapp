ff321e342ed4d593533f660a468e375614f0288f
Sam Taub <staub91@gmail.com>
2019-02-27 17:27:27 -0500

    cleaned up code

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -20,7 +20,6 @@ router.post("/api/burgers", function (req, res) {
     ], [
             req.body.name, false
         ], function (result) {
-            // Send back the ID of the new quote
             res.json({ id: result.insertId });
         });
 });
@@ -34,7 +33,6 @@ router.put("/api/burgers/:id", function (req, res) {
         devoured: true
     }, condition, function (result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
